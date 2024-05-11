234a321ee8e5aec060f0e401cab2fe30337d7dbd
jimmy <jimmy@github.com>
2024-05-11 16:24:10 +0800

    init

--- center/src/main/java/com/jimmy/friday/center/core/schedule/block/Serial.java
+++ center/src/main/java/com/jimmy/friday/center/core/schedule/block/Serial.java
@@ -6,10 +6,13 @@ import com.jimmy.friday.boot.enums.schedule.BlockHandlerStrategyTypeEnum;
 import com.jimmy.friday.center.Schedule;
 import com.jimmy.friday.center.base.schedule.Block;
 import com.jimmy.friday.center.entity.ScheduleJob;
+import lombok.Data;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
@@ -20,7 +23,7 @@ public class Serial implements Block {
 
     private final ConcurrentMap<Long, Thread> threadMap = Maps.newConcurrentMap();
 
-    private final ConcurrentMap<Long, Queue<ScheduleJob>> queueMap = Maps.newConcurrentMap();
+    private final ConcurrentMap<Long, Queue<WaitInvoke>> queueMap = Maps.newConcurrentMap();
 
     @Autowired
     private Schedule schedule;
@@ -35,28 +38,34 @@ public class Serial implements Block {
         Long id = scheduleJob.getId();
         //放入队列
         queueMap.computeIfAbsent(id, i -> new ConcurrentLinkedQueue<>());
-        queueMap.get(id).add(scheduleJob.clear());
+        queueMap.get(id).add(new WaitInvoke(scheduleJob.clear(), currentShardingNum));
 
         if (!threadMap.containsKey(id)) {
             Thread thread = new Thread(() -> {
                 while (true) {
-                    Queue<ScheduleJob> queue = queueMap.get(id);
+                    Queue<WaitInvoke> queue = queueMap.get(id);
                     if (queue == null) {
+                        queueMap.remove(id);
                         break;
                     }
 
-                    if (schedule.isRunning(scheduleJob.getId(), currentShardingNum)) {
-                        ThreadUtil.sleep(1000);
-                        continue;
-                    }
-
-                    ScheduleJob poll = queue.poll();
+                    WaitInvoke poll = queue.poll();
                     if (poll == null) {
                         ThreadUtil.sleep(100);
                         continue;
                     }
 
-                    schedule.submit(poll, currentShardingNum);
+                    try {
+                        if (schedule.isRunning(id, poll.getCurrentShardingNum())) {
+                            queue.add(poll);
+                            ThreadUtil.sleep(1000);
+                            continue;
+                        }
+
+                        schedule.submit(poll.getScheduleJob(), poll.getCurrentShardingNum());
+                    } catch (Exception ignore) {
+
+                    }
                 }
             });
 
@@ -70,5 +79,18 @@ public class Serial implements Block {
     @Override
     public BlockHandlerStrategyTypeEnum type() {
         return BlockHandlerStrategyTypeEnum.SERIAL;
+    }
+
+    @Getter
+    private static class WaitInvoke implements Serializable {
+
+        private final ScheduleJob scheduleJob;
+
+        private final Integer currentShardingNum;
+
+        public WaitInvoke(ScheduleJob scheduleJob, Integer currentShardingNum) {
+            this.scheduleJob = scheduleJob;
+            this.currentShardingNum = currentShardingNum;
+        }
     }
 }
