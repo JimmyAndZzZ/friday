5b910d1016315446d9b8317bb4712c2a60abed51
jimmy <jimmy@github.com>
2024-06-06 16:02:04 +0800

    推送

--- client/src/main/java/com/jimmy/friday/client/Channel.java
+++ client/src/main/java/com/jimmy/friday/client/Channel.java
@@ -1,6 +1,8 @@
 package com.jimmy.friday.client;
 
+import cn.hutool.core.util.StrUtil;
 import com.jimmy.friday.boot.base.Listen;
+import com.jimmy.friday.boot.exception.GatewayException;
 import com.jimmy.friday.client.support.ChannelSupport;
 
 public class Channel {
@@ -14,6 +16,65 @@ public class Channel {
         SubBuilder subBuilder = new SubBuilder();
         subBuilder.channelName = channelName;
         return subBuilder;
+    }
+
+    public static StreamBuilder stream() {
+        return new StreamBuilder();
+    }
+
+    public static class StreamBuilder {
+
+        private String tableName;
+
+        private String database;
+
+        private String appId;
+
+        private String server;
+
+        public StreamBuilder setAppId(String appId) {
+            this.appId = appId;
+            return this;
+        }
+
+        public StreamBuilder setServer(String server) {
+            this.server = server;
+            return this;
+        }
+
+        public StreamBuilder setTableName(String tableName) {
+            this.tableName = tableName;
+            return this;
+        }
+
+        public StreamBuilder setDatabase(String database) {
+            this.database = database;
+            return this;
+        }
+
+        public void listen(Listen listen) {
+            if (StrUtil.isEmpty(database)) {
+                throw new GatewayException("数据源名未定义");
+            }
+
+            if (StrUtil.isEmpty(tableName)) {
+                throw new GatewayException("表名未定义");
+            }
+
+            ChannelSupport.sub(database + StrUtil.UNDERLINE + tableName, appId, listen, server);
+        }
+
+        public void cancel() {
+            if (StrUtil.isEmpty(database)) {
+                throw new GatewayException("数据源名未定义");
+            }
+
+            if (StrUtil.isEmpty(tableName)) {
+                throw new GatewayException("表名未定义");
+            }
+
+            ChannelSupport.cancelSub(database + StrUtil.UNDERLINE + tableName, appId, server);
+        }
     }
 
     public static class SubBuilder {
